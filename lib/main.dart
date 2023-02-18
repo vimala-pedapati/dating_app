8532427af9b2f079c9766fa1fdbcc28a89cde6e4
PremGanesh <premganesh2655@gmail.com>
2023-02-18 10:11:09 +0530

    checking push persmission

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-
+// checking push premissions
   // Initialized before calling runApp to init firebase app
   WidgetsFlutterBinding.ensureInitialized();
 
